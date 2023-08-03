b80a78251641b47517d80b8dc5252dcb08662db3
Melvin Quashie <melvinquashie@outlook.com>
2023-08-03 05:26:53 +0000

    Solved 11, Working on 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function() 
+{
+  function findOldestPerson(people) {
+    let oldestPerson = people[0];
+    let oldestAge = calculateAge(oldestPerson);
+  
+    for (let i = 1; i < people.length; i++) {
+      const currentPerson = people[i];
+      const currentAge = calculateAge(currentPerson);
+  
+      if (currentAge > oldestAge) {
+        oldestPerson = currentPerson;
+        oldestAge = currentAge;
+      }
+    }
+  
+    return oldestPerson;
+  }
+  
+  function calculateAge(person) {
+    const currentYear = new Date().getFullYear();
+    return person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : currentYear - person.yearOfBirth;
+  }
 };
 
 // Do not edit below this line
